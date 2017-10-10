fd39224a8cbb0f74ee172c78ba6b01b684159328
Tobias Schifftner <ts@ambimax.de>
2017-10-10 14:30:02 +0200

    Fixed unit test for default region

--- src/app/code/community/Ambimax/Import/Test/Helper/Aws.php
+++ src/app/code/community/Ambimax/Import/Test/Helper/Aws.php
@@ -33,14 +33,10 @@ class Ambimax_Import_Test_Helper_Aws extends Ambimax_Import_Test_Abstract
      */
     public function testDefaultRegion()
     {
-        $this->assertEquals('sa-east-1', Mage::getStoreConfig('ambimax_import/general/aws_default_region'));
-
         /** @var Ambimax_Import_Helper_Aws $helper */
         $helper = Mage::helper('ambimax_import/aws');
 
-        $client = $helper->getClient('default');
-        $this->assertInstanceOf('Aws\S3\S3Client', $client);
-        $this->assertNull($client->getRegion());
+        $this->assertEquals('sa-east-1', Mage::getStoreConfig('ambimax_import/general/aws_default_region'));
         $this->assertEquals('sa-east-1', $helper->getDefaultRegion());
     }
 
